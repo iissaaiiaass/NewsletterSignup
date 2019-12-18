fe856763345c6e87aeb9518585c94cd6717ad63b
iissaaiiaass <isaiasdl52@gmail.com>
2019-12-18 13:36:23 -0400

    set the application code for deployment.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
   console.log("Server use port 3000");
 });
 
